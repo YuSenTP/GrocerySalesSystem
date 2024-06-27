94b450ce403c448c5d38933e935c5d78da2333bc
Yu Sen <leeyusenv2@gmail.com>
2024-06-27 19:07:00 +0800

    Controller and data storage updated

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -3,12 +3,16 @@
  */
 package controller;
 
+import data.DataStorage;
+
 /**
  * @author Yu Sen
  *
  */
 public class Controller {
+	public DataStorage ds;
+	
 	public Controller(){
-		
+		 this.ds = new DataStorage();
 	}
 }
--- src/data/DataStorage.java
+++ src/data/DataStorage.java
@@ -3,17 +3,100 @@
  */
 package data;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Vector;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * @author Yu Sen
  *
  */
-public class DataStorage {
+
+public class DataStorage { //!! TO_CHANGE -- filePaths
 	
-	Vector<GroceryItem> inventory = new Vector<GroceryItem>();
-	Vector<Order> orders = new Vector<Order>();
-	Vector<Staff> staffs = new Vector<Staff>();
-	Vector<Manager> managers = new Vector<Manager>();
+	private Vector<GroceryItem> inventory;
+	private Vector<Order> orders;
+	private Vector<Staff> staffs;
+	private Vector<Manager> managers;
+	
+	public DataStorage(){
+		this.inventory = new Vector<GroceryItem>();
+		this.orders = new Vector<Order>();
+		this.staffs = new Vector<Staff>();
+		this.managers = new Vector<Manager>();
+		
+		this.readFile();
+	}
+	
+	public Vector<GroceryItem> getInventory(){
+		return this.inventory;
+	}
+	
+	public Vector<Order> getOrders(){
+		return this.orders;
+	}
+	
+	public Vector<Staff> getStaffs(){
+		return this.staffs;
+	}
+	
+	public Vector<Manager> getManagers(){
+		return this.managers;
+	}
+	
+	public void readFile(){ // Method to read from JSON file
+		ObjectMapper objectMapper = new ObjectMapper();
+		
+		try{
+			this.managers = objectMapper.readValue(
+	                 new File("./src/JsonReadWrite/startManager.json"),
+	                 new TypeReference<Vector<Manager>>(){}
+	         );
+			
+			this.staffs = objectMapper.readValue(
+	                 new File("./src/JsonReadWrite/startStaff.json"),
+	                 new TypeReference<Vector<Staff>>(){}
+	         );
+	        
+	         this.inventory = objectMapper.readValue(
+	                 new File("./src/JsonReadWrite/startinventory.json"),
+	                 new TypeReference<Vector<GroceryItem>>(){}
+	         );
+	         
+	         this.orders = objectMapper.readValue(
+	                 new File("./src/JsonReadWrite/emptyorder.json"),
+	                 new TypeReference<Vector<Order>>(){}
+	         );
+
+	         //Loops through staffs Vector and set each object index to staff
+	         for (Staff staff : this.staffs) { //set new instance of order to each staff -- current state of order: null
+//	             System.out.println(user.getName());
+	        	 staff.start();
+	        	 
+	         } 
+			
+		}catch (IOException e) {
+	         e.printStackTrace();
+	     }
+	}
+	
+	public void writeFile(){  //Method to save to JSON file
+		for (int i = 0; i < this.staffs.size(); i++){ //Clear staff order object to null
+			this.staffs.get(i).setOrder(null);
+		}
+    	ObjectMapper objectMapper = new ObjectMapper(); 
+    	try {
+    		//! TO-CHANGE filePath
+			objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File("./src/JsonReadWrite/inventory.json"), this.inventory);
+			objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File("./src/JsonReadWrite/manager.json"), this.managers);
+			objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File("./src/JsonReadWrite/staff.json"), this.staffs);
+			objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File("./src/JsonReadWrite/order.json"), this.orders);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+	}
 
 }
